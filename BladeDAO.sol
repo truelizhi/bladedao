7444754f301a0e01d6dc2ceba53ea5f6348744dd
lizhi <lxx1233@qq.com>
2023-05-06 01:09:18 +0800

    Update BladeDAO.sol

--- BladeDAO.sol
+++ BladeDAO.sol
@@ -7,17 +7,20 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract BladeDAO is ERC721 {
 
     bytes32 public immutable root = 0xe665f3df980b27e3b0ade8f15f922467f031537aead8455ba79e2440cfb0f18e; // Merkle树的根
+    uint private deployTime;
 
     // 构造函数，初始化NFT合集的名称、代号、Merkle树的根
     constructor() ERC721("BladeDAO", "ITM") {
-        
+        deployTime = block.timestamp;
     }
 
     function safeMint(
         address account,
         bytes32[] calldata proof
     ) external {
-        require(_verify(_leaf(account), proof), "Invalid merkle proof"); // Merkle检验通过
+        if(block.timestamp - deployTime < 60){
+            require(_verify(_leaf(account), proof), "Invalid merkle proof"); // Merkle检验通过
+        }
         _safeMint(account); // mint
     }
 
